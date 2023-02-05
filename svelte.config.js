d1e4556c6e29b307f86d9e3909005c58c194c01e
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-02-04 22:46:55 -0500

    Configure Svelte to run alongside Rust

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-auto"
+import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		files: {
+			routes: "ui/routes",
+			appTemplate: "ui/app.html",
+			hooks: {
+				client: "ui/hooks.client",
+				server: "ui/hooks.server",
+			},
+			lib: "ui/lib",
+			serviceWorker: "ui/service-worker",
+			errorTemplate: "ui/error.html",
+		},
+	},
+}
 
-export default config;
+export default config
